3eb9e607a72ac44e7ec5126a435b656648a7feb0
BP Mishra <tech@golivelabs.io>
2016-02-06 11:55:39 -0800

    GET POST PUT and DELETE service points are operational

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/chandu', function (err) {
+  if (err) console.error('There was an error connecting to mongodb!');
+  else console.log('Successfully connected to mongodb!');
+});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+/* Work with Contacts */
+var ContactsModel = mongoose.model('contact', {
+  name: String,
+  email: String,
+  phone: String
+});
+
+
+
+router.get('/contacts', function (req, res) {
+
+  ContactsModel.find(function (err, results) {
+    if (err) res.status(500).json({err: err});
+    else res.status(200).json({
+      results: results
+    });
+  });
+
+});
+
+
+router.post('/contacts', function (req, res) {
+
+  (new ContactsModel(req.body)).save(function (err, result) {
+    if (err) res.status(500).json({err: err});
+    else res.status(201).json({
+      result: result
+    });
+  });
+
+});
+
+router.put('/contacts/:id', function (req, res) {
+
+  ContactsModel.findByIdAndUpdate(req.params.id, req.body, function (err, result) {
+    if (err) res.status(500).json({err: err});
+    else res.status(200).json({
+      result: result
+    });
+  });
+
+});
+
+router.delete('/contacts/:id', function (req, res) {
+    ContactsModel.findByIdAndRemove(req.params.id, function (err, result) {
+      if (err) res.status(500).json({err: err});
+      else res.status(200).json({
+        result: result
+      });
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +60,16 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+
+
+
+// Create a new contact
+/*(new ContactsModel({
+ name: 'Chandrashekar Srinivasulu',
+ email: 'chandu@gmail.com',
+ phone: '112356898'
+ })).save(function (err, results) {
+ if (err) console.log(err);
+ else console.log(results);
+ });*/
